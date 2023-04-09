b5e07e53acb779d5585ef1d5a1b6c1e41b48d1d6
DELL <DELL@10.0.0.179>
2023-04-09 18:16:21 -0400

    GitHub homework 2

---
+++ src/Day2HW1/homework1.java
@@ -0,0 +1,18 @@
+package Day2HW1;
+
+public class homework1 {
+
+	public static void main (String args []) {
+		
+		
+		System.out.println("homework 1");
+		
+		
+		System.out.println("github homework number 2");
+		
+		
+	}
+	
+	
+	
+}
